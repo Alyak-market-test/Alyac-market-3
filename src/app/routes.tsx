e4fe2a7e7e3bed4b6a05350554b4eb2442b8bf77
youngjongkim <umaru19930407@gmail.com>
2026-02-19 15:42:16 +0900

    feat(widgets): BottomNav 및 RootLayout 구현

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { RootLayout } from '@/app/RootLayout';
 import { HomePage } from '@/pages/home';
 import { NotFoundPage } from '@/pages/not-found';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    // element: <RootLayout />,
+    element: <RootLayout />,
     children: [
       {
         index: true,

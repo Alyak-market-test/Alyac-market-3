76be5b1acbcbe1d5cb2979c6a0baf71c90cc8fca
youngjongkim <umaru19930407@gmail.com>
2026-02-19 14:57:34 +0900

    feat(pages): 404 페이지 구현 및 라우터 설정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { HomePage } from '@/pages/home';
+import { NotFoundPage } from '@/pages/not-found';
 
 export const router = createBrowserRouter([
   {
@@ -12,5 +13,9 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
   },
 ]);

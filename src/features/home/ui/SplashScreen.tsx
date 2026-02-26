68683dd4940e216f47b3748e2110df2240e6b573
Github Deco4710 <koong2230@gmail.com>
2026-02-26 10:03:03 +0900

    feat:hompage,searchpage 다크모드 추가

--- src/features/home/ui/SplashScreen.tsx
+++ src/features/home/ui/SplashScreen.tsx
@@ -2,7 +2,7 @@ import { LogoFullIcon } from '@/shared/icons/LogoFullIcon';
 
 export function SplashScreen() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-white">
+    <div className="bg-background flex h-screen w-full flex-col items-center justify-center">
       <LogoFullIcon width={199} height={220} />
     </div>
   );

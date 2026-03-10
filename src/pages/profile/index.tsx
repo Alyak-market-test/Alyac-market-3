f4d161340b47d053168d28d4390de2d5b405dc07
www1240kr-web <www.1240.kr@gmail.com>
2026-03-10 10:33:33 +0900

    fix: 프로필정보, 버튼 간격 수정

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -91,7 +91,7 @@ export function ProfilePage() {
         />
       </section>
 
-      <div className="-my-4">
+      <div className="-mt-4 -mb-2">
         {isMyProfile ? (
           <MyButtons />
         ) : (

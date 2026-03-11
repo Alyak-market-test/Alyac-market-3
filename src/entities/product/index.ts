2766df439905ff43933709929addb3a6485f4ab1
www1240kr-web <www.1240.kr@gmail.com>
2026-03-11 17:59:21 +0900

    refact: entities/product,post featrures/product 구조 개선

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -1,10 +1,6 @@
-export {
-  postProduct,
-  getProducts,
-  getProductDetail,
-  deleteProduct,
-  putProduct,
-} from './api/ProductApi';
-export { useGetProduct } from './model/useGetProduct';
-export { useGetProducts } from './model/useGetProducts';
+export { useGetProduct } from './hooks/useGetProduct';
+export { useGetProducts } from './hooks/useGetProducts';
+export { usePostProduct } from './hooks/usePostProduct';
+export { usePutProduct } from './hooks/usePutProduct';
+export { useDeleteProduct } from './hooks/useDeleteProduct';
 export type { Product, ProductPayload } from './model/ProductTypes';
--- src/features/post/post-add/model/usePostSubmit.ts
+++ src/features/post/post-add/model/usePostSubmit.ts
@@ -1,8 +1,9 @@
-import { uploadFiles } from '@/entities/image';
+import { useUploadFiles } from '@/entities/image';
 import { useCreatePost } from '@/entities/post';
 
 export function usePostSubmit() {
   const { mutate: createPost, isPending, error: mutationError } = useCreatePost();
+  const { mutateAsync: uploadFiles } = useUploadFiles();
 
   const mutate = async ({ content, imageFiles }: { content: string; imageFiles: File[] }) => {
     let imageString = '';
